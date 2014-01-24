5ccfc74fae3d38193160f0d1ff7388ef03ba76cb
Nayana <nayana@corp-gems.com>
2014-01-24 06:44:04 -0500

    changes to event

--- src/view/pane.js
+++ src/view/pane.js
@@ -1096,19 +1096,18 @@ Candy.View.Pane = (function(self, $) {
 			}
 			roomJid = Candy.Util.unescapeJid(roomJid);
 			var roomId = Candy.Util.jidToId(roomJid);
-			self.Chat.rooms[roomJid] = {id: roomId, usercount: 0, name: roomName, type: roomType, messageCount: 0, scrollPosition: -1};
 
 			var evtData = { jid: roomJid };
-			
-			console.log("Trigger Room "  + isUserInit);
 			
 			//we need a trigger here before roomShow
 			$(Candy).triggerHandler('candy:view.room.before-add', evtData);
 			
 			if (evtData.block && isUserInit === false) {
-				console.log("trigger returned block true, and connection is remote aborting");
+				//console.log("trigger returned block true, and connection is remote aborting");
 				return false;
 			}
+			
+			self.Chat.rooms[roomJid] = {id: roomId, usercount: 0, name: roomName, type: roomType, messageCount: 0, scrollPosition: -1};
 			
 			$('#chat-rooms').append(Mustache.to_html(Candy.View.Template.Room.pane, {
 				roomId: roomId,
